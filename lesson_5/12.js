055c702548d1bbce7622cc0368b3104ab2b12511
Vladimir <vladimirloyanich@gmail.com>
2016-03-22 21:32:06 +0200

    fix bag idex masiv result

--- lesson_5/12.js
+++ lesson_5/12.js
@@ -1,20 +1,16 @@
 "use strict";
 var addresses = require('./addresses');
 var result = [];
-var reg =/(?: +)?(?:[а-я\-]+\.? ?)?((?:[0-9\-]+)?(?:[а-яА-Яё ])+?)(?:,?)(?: +)?(?:дом)?(?: +)?(\d+-?[а-яА-Яё]?)(?:[, ]+)?(?:кв\.|\/)?(?: +)?(\d+)?(?: +)?/g;
-debugger;
+var reg =/(?: +)?(?:[а-я\-]+\.? ?)?((?:[0-9\-]+)?(?:[а-яА-Яё ])+?)(?:,?)(?: +)?(?:дом)?(?: +)?(\d+-?[а-яА-Яё]?)(?:[, ]+)?(?:кв\.|\/)?(?: +)?(\d+)?(?: +)?/g;//why g flag?!!
 var el = [];
 for (var i=0;i<addresses.length;i++){
-    while (el = reg.exec(addresses[i])){
-        result.push(new (function (){
-            this["street"]=el[1];
-            this["house"]=el[2];
-            this["flat"]=(el[3]==undefined)?"":el[3];
-            return this;
-        })());
+    if(el= reg.exec(addresses[i])){
+        result.push(    {street:el[1],
+            house:(el[2] == undefined) ? "" : el[2],
+            flat:(el[3] == undefined) ? "" : el[3]  });
+        reg.exec();
+    }else{result.push("error parser")}
     }
-};
-console.log(result);
 module.exports = result;
 
 
